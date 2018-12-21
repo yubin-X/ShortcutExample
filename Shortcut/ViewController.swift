a93c22090dd9a43210c5afcc3c7db721aef2f2f1
Knight Zhang <jingyuan.zhang@vml.com>
2018-12-21 15:20:42 +0800

    Initial Commit

---
+++ Shortcut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shortcut
+//
+//  Created by apple on 2018/12/21.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
